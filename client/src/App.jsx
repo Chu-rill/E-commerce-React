e8e06ab2f075d3717db9d7767b742d890897c72d
Churchill <churchillrimamskep@gmail.com>
2024-04-07 15:12:22 +0100

    added light and dark mode toggle

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 import { Button } from "./components/ui/button";
-
+import ThemeToggleButton from "./components/ThemeToggleButton";
 function App() {
   return (
     <>
       <Button>Heloo World</Button>
+      <ThemeToggleButton />
     </>
   );
 }
